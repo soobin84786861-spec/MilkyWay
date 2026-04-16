8ab85ad4af53e54d844c0ad6c622c686aab8d96b
thewayhj <tnwndvhrrurr@paran.com>
2026-04-16 21:24:07 +0900

    allowedHosts 정보 추가.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: [
+      'dev-milkyway-alb-746196076.ap-northeast-2.elb.amazonaws.com'
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
